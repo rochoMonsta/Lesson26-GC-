6fe424fd7bd62757cad3e81edf85bc50f0667315
rochoMonsta <42835126+rochoMonsta@users.noreply.github.com>
2020-02-26 18:53:04 +0200

    git add

---
+++ Lesson26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Виведення задіяної пам'яті.
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            for (int i = 0; i < 10000; ++i)
+            {
+                var obj = (object)i;
+                int j = (int)obj;
+            }
+            Console.WriteLine(GC.GetTotalMemory(false));
+            //Очищення пам'яті. (перегрузки (вибір очистки по рівню)).
+            GC.Collect();
+            Console.WriteLine(GC.GetTotalMemory(false));
+            Console.ReadLine();
+        }
+    }
+}
